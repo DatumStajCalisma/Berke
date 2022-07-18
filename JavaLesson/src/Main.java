84bdcc14b7a19443b383de7e92cf61bcb1f12912
kaanberke34 <berkeyilmaz34@gmail.com>
2022-07-18 16:39:19 +0300

    day 1

---
+++ JavaLesson/src/Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	    
+		   //String test1 = "text1";
+		   //Boolean test2 = true;
+		    
+		    // Double number1 = 22.5;
+		    // Double number2 = 10.2;
+		    // if(number1<number2){
+		    // System.out.println("number1 is low, number2 is high'");
+		    // } else if (number1>number2){
+		    //    System.out.println("number1 is high,number2 is low");
+		    // }else {
+		    //   System.out.println("numbers are equal");
+		    // }
+
+		String[] texts = {
+		  "text1",
+		  "text2",
+		  "text3",
+		  "text4",
+		  "text5"
+		};
+		    // for (String text : texts) {
+		    //   System.out.println(text);
+		    // };
+
+		    for (int i = 0; i<texts.length; i++){
+		System.out.println(texts[i]);
+		      
+		    }
+
+	}
+
+}
