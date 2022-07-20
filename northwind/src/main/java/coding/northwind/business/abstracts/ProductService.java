de5ba47560317a1af99cfb626a2a42d766fae5bc
kaanberke34 <berkeyilmaz34@gmail.com>
2022-07-20 15:51:29 +0300

    Testing api, spring boot application

---
+++ northwind/src/main/java/coding/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,10 @@
+package coding.northwind.business.abstracts;
+
+import java.util.List;
+
+import coding.northwind.entities.concretes.Product;
+
+public interface ProductService {
+	
+	List<Product> getAll();
+}
