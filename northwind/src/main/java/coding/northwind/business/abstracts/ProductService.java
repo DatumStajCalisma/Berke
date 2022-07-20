c6738f096597bcdc5faee59969b5322118429626
kaanberke34 <berkeyilmaz34@gmail.com>
2022-07-20 17:48:40 +0300

    day 3

--- northwind/src/main/java/coding/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/coding/northwind/business/abstracts/ProductService.java
@@ -2,9 +2,13 @@ package coding.northwind.business.abstracts;
 
 import java.util.List;
 
+import coding.northwind.core.utilities.results.DataResult;
+import coding.northwind.core.utilities.results.Result;
 import coding.northwind.entities.concretes.Product;
 
 public interface ProductService {
 	
-	List<Product> getAll();
+	DataResult<List<Product>> getAll();
+
+	Result add(Product product);
 }
